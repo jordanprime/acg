44186e151f7d6f60692494193e99245e00e09386
Jordan Prime <jordanprime@me.com>
2024-06-03 15:50:59 +0930

    rdfgdfsgsfdgrgerg

---
+++ test/AnonymousUserTests.java
@@ -0,0 +1,85 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AnonymousUserTests {
+
+    @Test
+    @DisplayName("Adding a user without authentication.")
+    void anonymousAddUser() {
+
+        final String username = "john_doe";
+        final String name = "John Doe";
+        final String password = "password123";
+
+        LMS lms = new LMS();
+        Student student1 = new Student(username, password, name);
+        lms.addUser(student1);
+
+    }
+
+    @Test
+    @DisplayName("Adding an exam grade for a student without authentication.")
+    void anonymousAddExamGrade() {
+
+        String username = "john_doe";
+        LMS lms = new LMS();
+        lms.addExamGrade(username, "HD");
+
+    }
+
+    @Test
+    @DisplayName("Getting all exam grades without authentication.")
+    void anonymousGetGrades() {
+
+        LMS lms = new LMS();
+        lms.getExamGrades();
+
+    }
+
+    @Test
+    @DisplayName("Getting all assignments without authentication.")
+    void anonymousGetAssignments() {
+
+        LMS lms = new LMS();
+        String username = "john_doe";
+        lms.uploadAssignment(username, "Assignment 6");
+        assertEquals("{john_doe=Assignment 6}", lms.getAssignments().toString());
+
+    }
+
+    @Test
+    @DisplayName("Uploading an assignment without authentication.")
+    void anonymousUploadAssignment() {
+
+        LMS lms = new LMS();
+        String username = "john_doe";
+        lms.uploadAssignment(username, "Assignment 6");
+
+    }
+
+    @Test
+    @DisplayName("Add and get academic calendar without authentication.")
+    void anonymousAcademicCalendar() {
+
+        LMS lms = new LMS();
+        String username = "john_doe";
+        lms.addEventToCalendar("2024-06-03", "A really great event");
+        assertEquals("{2024-06-03=A really great event}", lms.getAcademicCalendar().toString());
+
+    }
+
+    @Test
+    @DisplayName("Add and get course materials without authentication.")
+    void anonymousCourseMaterials() {
+
+        LMS lms = new LMS();
+        String username = "john_doe";
+        lms.addCourseMaterial("Excellent course material");
+        assertEquals("[Excellent course material]", lms.getCourseMaterials().toString());
+
+    }
+
+}
